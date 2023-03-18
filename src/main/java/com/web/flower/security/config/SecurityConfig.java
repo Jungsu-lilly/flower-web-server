49add8a152949f8ee86fb89fb566fb1e72070ec9
matt <matt1235@naver.com>
2023-03-18 20:18:03 +0900

    fix : Preflight 요청 시 허용하는 로직 필터에 추가 -> CORS 에러 수정

--- src/main/java/com/web/flower/security/config/SecurityConfig.java
+++ src/main/java/com/web/flower/security/config/SecurityConfig.java
@@ -23,6 +23,7 @@ import org.springframework.security.web.context.SecurityContextRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.http.HttpServletResponse;
@@ -55,6 +56,7 @@ public class SecurityConfig {
 
         http
                 .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .antMatchers("/api/social-login/**", "/api/login", "/api/user/one", "/api/user/logout")
                 .permitAll()
                 .antMatchers("/api/**")
